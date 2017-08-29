5502c5748a17f8f32cc40735cd545b9aff2989dc
bmac <bmac325@gmail.com>
2017-08-29 16:38:42 -0400

    Start with some basic structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css';
+import Index from './routes/index'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <BrowserRouter>
+        <div className="App">
+          <header>Header</header>
+          <main>
+            <Route path="/" component={Index} exact />
+          </main>
+          <footer>footer</footer>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class IndexPage extends Component {
+  render() {
+    return (
+      <div>
+        list all of the podcasts
+      </div>
+    );
+  }
+}
+
+export default IndexPage;
