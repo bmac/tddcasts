96b412c4218a270af33e3f0eb48b146fd4763505
bmac <bmac325@gmail.com>
2017-09-04 15:54:18 -0400

    Clean up lint warnings

--- src/routes/home/components/episode.js
+++ src/routes/home/components/episode.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Episode = ({episode, playEpisode}) => {
   return (
@@ -9,7 +9,7 @@ const Episode = ({episode, playEpisode}) => {
         <div className="episode-title">{episode.title}</div>
       </div>
       <div className="episode-duration">{episode.duration}</div>
-      <button className="play-episode" role="button" onClick={playEpisode.bind(null, episode)}>Play</button>
+      <button className="play-episode" type="button" onClick={playEpisode.bind(null, episode)}>Play</button>
     </article>
   )
 }
